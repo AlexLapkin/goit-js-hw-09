5cabe487d9faecc5d4bc75ce85b86deb78800804
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2023-09-03 13:29:46 +0000

    Deploying to gh-pages from @ AlexLapkin/goit-js-hw-09@af6312be3a2d37f50420ccd65b84c7b8265ae554 🚀

--- 01-color-switcher.2534e1d2.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.body;t.addEventListener("click",(function(){setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.setAttribute("disabled",!0),e.removeAttribute("disabled")}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),e.setAttribute("disabled",!0),clearInterval(timerId)}))}();
-//# sourceMappingURL=01-color-switcher.2534e1d2.js.map
